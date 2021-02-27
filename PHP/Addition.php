70c327dc1036f46e3c23a12b3003d4769b15559f
onlitstechnologies <onlitstechnologies@gmail.com>
2021-02-27 16:11:07 +0530

    20210227

---
+++ PHP/Addition.php
@@ -0,0 +1,12 @@
+<?php
+$a = readline("Enter a number: ");
+$b = readline("Enter another number: ");
+
+$c = $a + $b;
+
+echo "The sum is $c\n";
+
+
+#Dynamically types
+#Formatted String
+#printf("The sum is %d\n.",c);
